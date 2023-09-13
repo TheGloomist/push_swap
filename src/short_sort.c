8cb87b967caf10bbc83fbfe99534ac13da36e3be
TheGloomist <woodchips.picture@gmail.com>
2023-09-13 17:48:19 +0200

    Add minor changes to the layout and some TODO comments for clarity
    -m
    exit
    ''

--- src/short_sort.c
+++ src/short_sort.c
@@ -6,12 +6,14 @@
 /*   By: izaitcev <izaitcev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/17 14:17:41 by izaitcev      #+#    #+#                 */
-/*   Updated: 2023/05/19 16:48:50 by izaitcev      ########   odam.nl         */
+/*   Updated: 2023/09/13 17:42:42 by izaitcev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+// TODO: change this function to return actual lowest number
+//       instead of looking for 0
 int	find_lowest_nbr(t_data *d,t_link **stack)
 {
 	int		stack_size;
@@ -38,19 +40,30 @@ void	sort_rest(t_data *d)
 {
 	int	stack_size;
 	int	lowest_nbr_i;
+	int	i;
 
 	stack_size = ps_lstsize(d->stack_a);
 	lowest_nbr_i = find_lowest_nbr(d, d->stack_a);
+	i = 0;
+	if (lowest_nbr_i >= (stack_size / 2))
+	{
+		while (lowest_nbr_i < stack_size)
+		{
+			rra(d);
+			lowest_nbr_i++;
+		}
+	}
 	if (lowest_nbr_i < (stack_size / 2))
 	{
-		rra(d);
-		lowest_nbr_i++;
+		while (lowest_nbr_i )
+
 	}
 
 	
 
 }
 
+// TODO : change sorting 3 elements to work with any number
 void	sort_three(t_data *d)
 {
 	if (d->stack_a->content == 0)
@@ -77,13 +90,21 @@ void	sort_three(t_data *d)
 	}
 }
 
-void	short_sort(t_data *d)
+void	sorting(t_data *d)
 {
 	int	i;
+	int	stack_len;
 
 	i = 0;
-	if (ps_lstsize(d->stack_a) <= 3 && !is_sorted(d))
-		sort_three(d);
-	else if (ps_lstsize(d->stack_a) > 3 && !is_sorted(d))
-		sort_rest(d);
+	stack_len = ps_lstsize(d->stack_a);
+
+	if (!is_sorted(d))
+	{
+		if (stack_len > 60)
+			radix();
+		else if (stack_len <= 3)
+			sort_three(d);
+		else if (stack_len > 3 && stack_len < 60)
+			sort_rest(d);
+	}
 }
