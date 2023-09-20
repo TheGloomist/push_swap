fd6d9c4f2c9e0b091d6b784894063396543098cf
TheGloomist <woodchips.picture@gmail.com>
2023-09-20 17:16:58 +0200

    Add minor changes to sorting three elements, although I'm still figuring out how exactly I want to do it. Also small changes to finding lowest number for short sort, work in progress as well

--- src/short_sort.c
+++ src/short_sort.c
@@ -6,7 +6,7 @@
 /*   By: izaitcev <izaitcev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/17 14:17:41 by izaitcev      #+#    #+#                 */
-/*   Updated: 2023/09/13 17:42:42 by izaitcev      ########   odam.nl         */
+/*   Updated: 2023/09/20 17:14:14 by izaitcev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,24 @@
 
 // TODO: change this function to return actual lowest number
 //       instead of looking for 0
-int	find_lowest_nbr(t_data *d,t_link **stack)
+int	find_lowest_nbr(t_data *d,t_link **stack, int stack_size)
 {
-	int		stack_size;
 	int		i;
+	int		lowest_num;
 	t_link	*temp;
 
-	stack_size = ps_lstsize(d->stack_a);
 	i = 0;
+	lowest_num = 0;
 	temp = d->stack_a;
 	while (i < stack_size)
 	{
-		if(temp->content != 0)
+		if(temp->content < temp)
 		{
 			i++;
 			temp = temp->next;
 		}
-		else
-			break ;
 	}
-	return (i);
+	return (lowest_num);
 }
 
 void	sort_rest(t_data *d)
@@ -43,7 +41,7 @@ void	sort_rest(t_data *d)
 	int	i;
 
 	stack_size = ps_lstsize(d->stack_a);
-	lowest_nbr_i = find_lowest_nbr(d, d->stack_a);
+	lowest_nbr_i = find_lowest_nbr(d, d->stack_a, stack_size);
 	i = 0;
 	if (lowest_nbr_i >= (stack_size / 2))
 	{
@@ -66,28 +64,36 @@ void	sort_rest(t_data *d)
 // TODO : change sorting 3 elements to work with any number
 void	sort_three(t_data *d)
 {
-	if (d->stack_a->content == 0)
-	{
-		rra(d);
-		sa(d);
-	}
-	else if (d->stack_a->content == 1)
-	{
-		if (d->stack_a->next->content == 0)
-			sa(d);
-		else
-			rra(d);
-	}
-	else if (d->stack_a->content == 2)
-	{
-		if (d->stack_a->next->content == 0)
-			ra(d);
-		else
-		{
-			sa(d);
-			rra(d);
-		}
-	}
+	int	top;
+	int	mid;
+	int	bot;
+
+	top = d->stack_a->content;
+	mid = d->stack_a->next->content;
+	bot = d->stack_a->next->next->content;
+
+	// if (d->stack_a->content == 0)
+	// {
+	// 	rra(d);
+	// 	sa(d);
+	// }
+	// else if (d->stack_a->content == 1)
+	// {
+	// 	if (d->stack_a->next->content == 0)
+	// 		sa(d);
+	// 	else
+	// 		rra(d);
+	// }
+	// else if (d->stack_a->content == 2)
+	// {
+	// 	if (d->stack_a->next->content == 0)
+	// 		ra(d);
+	// 	else
+	// 	{
+	// 		sa(d);
+	// 		rra(d);
+	// 	}
+	// }
 }
 
 void	sorting(t_data *d)
@@ -101,7 +107,7 @@ void	sorting(t_data *d)
 	if (!is_sorted(d))
 	{
 		if (stack_len > 60)
-			radix();
+			ft_radix_sort(d);
 		else if (stack_len <= 3)
 			sort_three(d);
 		else if (stack_len > 3 && stack_len < 60)
